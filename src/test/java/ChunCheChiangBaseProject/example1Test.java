64789a150b38cc315736637db20cce6a7082bf06
ChiangChunChe <d0693409@mail.fcu.edu.tw>
2017-12-06 12:02:27 +0800

    Commit Triangle

---
+++ src/test/java/ChunCheChiangBaseProject/example1Test.java
@@ -0,0 +1,12 @@
+package ChunCheChiangBaseProject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class example1Test {
+    @Test
+    public void getType() throws Exception {
+    }
+
+}
