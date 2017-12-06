dd0db5bf22a0298fc4dac06bfee8ccac5bed55e8
ChiangChunChe <d0693409@mail.fcu.edu.tw>
2017-12-06 12:38:19 +0800

    Commit Triangle

--- src/test/java/ChunCheChiangBaseProject/example1Test.java
+++ src/test/java/ChunCheChiangBaseProject/example1Test.java
@@ -8,8 +8,9 @@ import static org.junit.Assert.*;
 public class example1Test {
     @Test
     public void getType() throws Exception {
-        example1 a=new example1(10,50,10);
-        System.out.println(a);
+        example1 re=new example1(10,50,10);
+
+        System.out.println(re.getType());
     }
 
 }
